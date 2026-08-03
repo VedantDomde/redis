8a5edae7aafc304b4e2e2eee2e80068e3fa4d63f
Vedant Domde <vedantdomde28@gmail.com>
2026-08-03 11:32:27 +0530

    add unit tests for sum, sub, and multiply functions

--- --test--/dummy.test.js
+++ --test--/dummy.test.js
@@ -1,8 +1,20 @@
-import {sum} from "../utils";
+import {sum,sub,multiply} from "../utils";
 
 describe('sum function', () => {
     test('add 1 + 2 to equal 3 ', () => {
         // Move the closing parenthesis right after sum(1, 2)
         expect(sum(1, 2)).toBe(3); 
+    });
+});
+
+describe('sub function',() => {
+    test('sub 5 - 2 to equal 3', () => {
+        expect(sub(5,2)).toBe(3);
+    });
+});
+
+describe('multiply function',() => {
+    test('multiply 2 * 3 to equal 6', () => {
+        expect(multiply(2,3)).toBe(6);
     });
 });
