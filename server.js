6819ee7af166c52b4a8ead60c4054565c5191331
Vedant Domde <vedantdomde28@gmail.com>
2026-08-04 15:49:35 +0530

    fix: remove redundant comments in server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+// use this middleware to parse incoming JSON request bodies
+// 
 
 // Middleware to parse incoming JSON request bodies
 app.use(express.json());
